3fcffde47970190c8e30b7de5cc16d86b14ab71e
dattadhanasure21 <dattadhanasure21@gmail.com>
2022-08-05 18:34:07 +0530

    first commit

---
+++ src/main/java/in/ashokit/SendAppStartMail.java
@@ -0,0 +1,12 @@
+package in.ashokit;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class SendAppStartMail implements CommandLineRunner {
+@Override
+public void run(String... args) throws Exception {
+	// TODO Auto-generated method stub
+	System.out.println("logic executing to send email....");
+}
+}
